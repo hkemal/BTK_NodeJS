bf0beeb3f9341884ad96295022954c45696736e9
hkemal <hasankemal96@gmail.com>
2023-05-18 20:33:32 +0300

    02-07-lecture

---
+++ 02-NodeJS/07-FileSystem-Modulu/app.js
@@ -0,0 +1,49 @@
+const fs = require("fs");
+
+// const files = fs.readdir("./", function (error, data) {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log(data);
+//   }
+// });
+
+// const data = fs.readFile("index.html", "utf-8", (error, data) => {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log(data);
+//   }
+// });
+
+// fs.writeFile("deneme.txt", "Hello World...", (error) => {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log("Dosya oluşturuldu");
+//   }
+// });
+
+// fs.appendFile("deneme1.txt", "Hello World...", (error) => {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log("Dosya oluşturuldu");
+//   }
+// });
+
+// fs.unlink("deneme1.txt", (error) => {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log("Dosya silindi");
+//   }
+// });
+
+// fs.rename("deneme.txt", "deneme1.txt", (error) => {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log("Dosya ismi değiştirildi");
+//   }
+// });
